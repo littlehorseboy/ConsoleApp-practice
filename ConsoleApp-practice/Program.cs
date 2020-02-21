95596e84e9d22388dc4a5fe4fbbaf5368dee3fe9
littlehorseboy <littlehorseboy@gmail.com>
2020-02-22 01:05:55 +0800

    加入專案檔案。

---
+++ ConsoleApp-practice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請問你的名字？");
+            string username = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine($"你好，{username}");
+        }
+    }
+}
